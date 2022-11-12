7abf2efeb3c8e3c3d17a806a9507b371ed6de755
pritesh <priteshmalviya32@gmail.com>
2022-11-12 13:52:17 +0530

    first commit

---
+++ marksheet.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a=0;
+    int rollnumber=93280799;
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                               |\n");
+    printf("|                        RAJIV GHANDHI PROUDYOGIKI VISHWAVIDHYALAYA,BHOPAL                      |\n");
+    printf("|                         (University of Technology of Madhya Pradesh)                          |\n");
+    printf("|                                                                                               |\n");
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                               |\n");
+    printf("|                                    EXAMINATION FEBRUARY-2022                                  |\n");
+    printf("|                                                                                               |\n");
+    printf("| ROLL NO.      : %d                                              MCA,MCA                 |\n",rollnumber);
+    printf("|                                                                                               |\n");
+    printf("| NAME          : PRITESH                                               SEMESTER  :  I          |\n");
+    printf("|                                                                                               |\n");
+    printf("| INSTITUTE     : IPS ACADMY                                            STATUS\t  :  REGULAR    |\n");
+    printf("|                                                                                               |\n");
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+
+
+    printf("|    CODE    |                   SUBJECTS          |               GRADING SYSTEM               |\n");
+    printf("|            |                                     |                                            |\n");
+
+
+    printf("|            |                                     |   TOTAL CREADIT   EARNED CREADIT  GRADE    |\n");
+    printf("|    102     |   Statistical Mathematics[T]        |         4                4          A+     |\n");
+    printf("|    101     |   Programing in C with DS[T]        |         4                4          A+     |\n");
+    printf("|    103     |  OperatingSystem & Architecture[T]  |         4                4          A+     |\n");
+    printf("|    104     |   Information Technology[T]         |         4                4          A+     |\n");
+    printf("|    105     |   Communication Skills[T]           |         4                4          A+     |\n");
+    printf("|    106     |    Programing in C with DS[P]       |         8                8          A+     |\n");
+    printf("|    107     |  OperatingSystem & Architecture[P]  |         2                2          A+     |\n");
+    printf("|            |                                     |                                            |\n");
+
+
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                               |\n");
+    printf("|                                                                            SPA  :  10         |\n",a);
+    printf("|                                                                                               |\n");
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                               |\n");
+    printf("| RESULT  :  PASS                                                           DIVISION  : I       |\n");
+    printf("| NOTE    :  It is computer genarated output                                                    |\n");
+    printf("|                                                                                               |\n");
+    printf("+-----------------------------------------------------------------------------------------------+\n");
+    getch();
+}
